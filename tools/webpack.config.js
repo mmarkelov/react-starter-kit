9b783e50975a90881afc8cb5fb0f0e01d8919b93
mapkeji <mapkeji26@gmail.com>
2018-09-23 14:39:30 +0300

    Add babel plugins

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -111,6 +111,8 @@ const config = {
             ['@babel/preset-react', { development: isDebug }],
           ],
           plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-syntax-dynamic-import',
             // Treat React JSX elements as value types and hoist them to the highest scope
             // https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-react-constant-elements
             ...(isDebug ? [] : ['@babel/transform-react-constant-elements']),
