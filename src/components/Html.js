f82bab6228355f2b56e6e559a0358e3e334de869
mapkeji <mapkeji26@gmail.com>
2018-12-07 15:38:46 +0300

    Make Html component stateless

--- src/components/Html.js
+++ src/components/Html.js
@@ -14,66 +14,60 @@ import config from '../config';
 
 /* eslint-disable react/no-danger */
 
-class Html extends React.Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    scripts: PropTypes.arrayOf(PropTypes.string.isRequired),
-    app: PropTypes.object, // eslint-disable-line
-    children: PropTypes.object.isRequired, // eslint-disable-line
-  };
+const Html = ({ title, description, scripts, app, children }) => (
+  <html className="no-js" lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {scripts.map(script => (
+        <link key={script} rel="preload" href={script} as="script" />
+      ))}
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="apple-touch-icon" href="/icon.png" />
+    </head>
+    <body>
+      <div id="app">{children}</div>
+      <script
+        dangerouslySetInnerHTML={{ __html: `window.App=${serialize(app)}` }}
+      />
+      {scripts.map(script => (
+        <script key={script} src={script} />
+      ))}
+      {config.analytics.googleTrackingId && (
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              'window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;' +
+              `ga('create','${
+                config.analytics.googleTrackingId
+              }','auto');ga('send','pageview')`,
+          }}
+        />
+      )}
+      {config.analytics.googleTrackingId && (
+        <script
+          src="https://www.google-analytics.com/analytics.js"
+          async
+          defer
+        />
+      )}
+    </body>
+  </html>
+);
 
-  static defaultProps = {
-    scripts: [],
-  };
+Html.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  scripts: PropTypes.arrayOf(PropTypes.string.isRequired),
+  app: PropTypes.object, // eslint-disable-line
+  children: PropTypes.object.isRequired, // eslint-disable-line
+};
 
-  render() {
-    const { title, description, scripts, app, children } = this.props;
-
-    return (
-      <html className="no-js" lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <title>{title}</title>
-          <meta name="description" content={description} />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          {scripts.map(script => (
-            <link key={script} rel="preload" href={script} as="script" />
-          ))}
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="apple-touch-icon" href="/icon.png" />
-        </head>
-        <body>
-          <div id="app">{children}</div>
-          <script
-            dangerouslySetInnerHTML={{ __html: `window.App=${serialize(app)}` }}
-          />
-          {scripts.map(script => (
-            <script key={script} src={script} />
-          ))}
-          {config.analytics.googleTrackingId && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html:
-                  'window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;' +
-                  `ga('create','${
-                    config.analytics.googleTrackingId
-                  }','auto');ga('send','pageview')`,
-              }}
-            />
-          )}
-          {config.analytics.googleTrackingId && (
-            <script
-              src="https://www.google-analytics.com/analytics.js"
-              async
-              defer
-            />
-          )}
-        </body>
-      </html>
-    );
-  }
-}
+Html.defaultProps = {
+  scripts: [],
+};
 
 export default Html;
