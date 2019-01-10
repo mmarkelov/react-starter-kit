f2f50813b1007182b2b84ba8bc3cbd7cc44a3bb2
mmarkelov <mapkeji26@gmail.com>
2019-01-10 16:29:13 +0300

    CountUp-Bug

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -8,75 +8,19 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import CountUp from 'react-countup';
 
 const RootDiv = styled.div`
   padding-left: 20px;
   padding-right: 20px;
+  text-align: center;
 `;
 
-const Container = styled.div`
-  margin: 0 auto;
-  padding: 0 0 40px;
-  max-width: ${props => props.theme.maxContentWidth};
-`;
-
-const NewsItem = styled.article`
-  margin: 0 0 2rem;
-`;
-
-const NewsTitle = styled.h1`
-  font-size: 1.5rem;
-`;
-
-const NewsDesc = styled.div`
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-size: 1.125rem;
-  }
-
-  pre {
-    white-space: pre-wrap;
-    font-size: 0.875rem;
-  }
-
-  img {
-    max-width: 100%;
-  }
-`;
-
-const Home = ({ news }) => (
+const Home = () => (
   <RootDiv>
-    <Container>
-      <h1>React.js News</h1>
-      {news.map(item => (
-        <NewsItem key={item.link}>
-          <NewsTitle>
-            <a href={item.link}>{item.title}</a>
-          </NewsTitle>
-          <NewsDesc
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: item.content }}
-          />
-        </NewsItem>
-      ))}
-    </Container>
+    <CountUp end={100} />
   </RootDiv>
 );
-
-Home.propTypes = {
-  news: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
-      content: PropTypes.string,
-    }),
-  ).isRequired,
-};
 
 export default Home;
